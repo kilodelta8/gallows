436d76ac774c11eef5971173984211d87e168d46
kilodelta8 <kilo.kilo.delta8@gmail.com>
2019-02-28 23:30:36 -0600

    Too much added to list, work on <enter> func for text entry

--- hangGui.py
+++ hangGui.py
@@ -3,7 +3,18 @@ from tkinter import ttk
 from randomwordgenerator import randomwordgenerator
 import random
 
+def setGeussVar():
+    letter = textEntry.get()
+    textEntry.delete(0, 'end')
+    geussLetter.set(letter)
 
+def play(word, geussWord):
+    geuss = str(input("Enter a letter: "))
+    count = 0
+    for x in word:
+        if geuss == x:
+            geussWord[count] = geuss
+        count += 1
 
 def randomWord():
     try:
@@ -16,19 +27,19 @@ def randomWord():
         randNum = random.randrange(0, (len(backupList)))
         return backupList[randNum]
 
-def lettersGeussed():
-    x = randomWord()
-    y = len(x)
-    geussing = []
-    for z in range(y):
-        geussing.append("-")
-    geussedWord.set(geussing)
+#returns a list of "-", the size of the randomly selected word
+def hiddenWordList(selectedWord):
+    hideWord = []
+    for z in range(len(selectedWord)):
+        hideWord.append("-")
+    #geussedWord.set(geussing)
+    return hideWord
     
 
 #using an 8 column by 6 row grid??
 #images are 400x400 and 
 
-
+#GUIdev---------------------------------------------------------------<<<<<<<<<<<<
 root = Tk()
 root.title("HangPy")
 #setup the main frame to hold all widgets
@@ -38,29 +49,110 @@ mainFrame.grid(column=0, row=0, sticky=(N, S, E, W))
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 
-#vars
-geussedWord = StringVar()
+#vars for GUI
+hiddenWordVar = StringVar()#????
+#var to hold the amount of attempts
+geussLetter = StringVar()
+#var to hold wrong geussed letters
+wrongGeussesVar = StringVar()
+#var to hold the amount of attempts made
+failedAttempts = StringVar() #should this be intVar???
 
-
+####>>>>------------------------------------------------Buttons and Labels----<<<<<<<<
 #photos for game
-#gallows
+#gallows 1
 photo1 = PhotoImage(file='C:\dev\hangpy\\resources\\001.gif')
-photoPreview = ttk.Label(mainFrame, image=photo1)
+photo2 = PhotoImage(file='C:\dev\hangpy\\resources\\002.gif')
+photo3 = PhotoImage(file='C:\dev\hangpy\\resources\\003.gif')
+photo4 = PhotoImage(file='C:\dev\hangpy\\resources\\004.gif')
+photo5 = PhotoImage(file='C:\dev\hangpy\\resources\\005.gif')
+photo6 = PhotoImage(file='C:\dev\hangpy\\resources\\006.gif')
+photo7 = PhotoImage(file='C:\dev\hangpy\\resources\\007.gif')
+photo8 = PhotoImage(file='C:\dev\hangpy\\resources\\008.gif')
+photoPlayAgain = PhotoImage(file='C:\dev\hangpy\\resources\\playAgain.gif')
+photoYouLose = PhotoImage(file='C:\dev\hangpy\\resources\\youLost.gif')
+photoYouWin = PhotoImage(file='C:\dev\hangpy\\resources\\youWin.gif')
+#<<<------------------------------------------->>>
+photoPreview = ttk.Label(mainFrame, image=photo1)#
+#<<<------------------------------------------->>>
 photo1.image = photo1 # keep a reference!
-photoPreview.grid(column=0, row=0, columnspan=2, rowspan=2, sticky=W)
+photo2.image = photo2 # keep a reference!
+photo3.image = photo3 # keep a reference!
+photo4.image = photo4 # keep a reference!
+photo5.image = photo5 # keep a reference!
+photo6.image = photo6 # keep a reference!
+photo7.image = photo7 # keep a reference!
+photo8.image = photo8 # keep a reference!
+photoPlayAgain.image = photoPlayAgain # keep a reference!
+photoYouLose.image = photoYouLose # keep a reference!
+photoYouWin.image = photoYouWin # keep a reference!
+#<<<--------------------------------------------------------------->>>
+photoPreview.grid(column=0, row=0, columnspan=4, rowspan=4, sticky=W)#
+#<<<--------------------------------------------------------------->>>^^^
+
 #correct geussed letters
-#
-lettersGeussedCorrect = ttk.Label(mainFrame, textvariable=geussedWord)
+#displays the hidden word array
+lettersGeussedCorrect = ttk.Label(mainFrame, textvariable=hiddenWordVar)
 lettersGeussedCorrect.grid(column=0, row=5, columnspan=4, sticky=S)
-button1 = ttk.Button(mainFrame, text='setvar', command=lettersGeussed)
+lettersGeussedCorrect.config(font=("Courier", 44)) #<<----changes font size
+#button for a new game
+button1 = ttk.Button(mainFrame, text='New Game', command=None)
 button1.grid(column=5, row=5, sticky=E)
 
-    #label to diplay letters already geussed
-alreadyGeussedLetters = ttk.Label(mainFrame, text='This area is for already geussed letters')
-alreadyGeussedLetters.grid(column=5, row=0, sticky=N)
+#label to diplay attempts
+attemptedGeusses = ttk.Label(mainFrame, textvariable=failedAttempts)
+attemptedGeusses.grid(column=4, row=0, sticky=(N, W))
+#label to head letters already geussed
+alreadyGeussed = ttk.Label(mainFrame, text='You have already geussed these letters:')
+alreadyGeussed.grid(column=4, row=0, sticky=(S, W))
+#displays all the letters geussed that are wrong
+wrongGeusses = ttk.Label(mainFrame, textvariable=wrongGeussesVar)
+wrongGeusses.grid(column=5, row=1)
+wrongGeusses.config(font=("Courier", 44)) #<<--------changes font size
+#
+#label to display previous geusses
+#text entery to submit geusses
+textEntry = ttk.Entry(mainFrame, textvariable=setGeussVar)
+#textEntry.bind(<Enter>, enter)<<<------WTF?!?!?!?!
+textEntry.grid(column=4, row=2, sticky=W)
 
 
+
+
+
+#Game logic/loop
+#
+#get random word and store it in a list
+randWord = randomWord()
+print(randWord)
+
+#initialize the hidden word list
+hiddenWord = hiddenWordList(randWord)
+print(hiddenWord)
+
+#set the maximum allowed geusses (per game)
+maxGeusses = len(randWord) - 1
+print(maxGeusses)
 root.mainloop()
+geusses = 0
+count = 0
+while hiddenWord != randomWord:
+    wrongLetters = []
+    #update hidden array here??
+    if geusses < maxGeusses:
+        hiddenWordVar.set(hiddenWord)
+        wrongGeussesVar.set(wrongLetters)
+        failedAttempts.set(geusses)
+        #letter = geussLetter.get()
+        for x in randWord:
+            if letter == x:
+                hiddenWord[count] = letter
+            else:
+                wrongLetters.append(letter)
+                geusses += 1
+        count += 1
+
+#root.mainloop()
 
 
 
